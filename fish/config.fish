9468958a135e2227e1d44eb967c1bf15964777c1
Peter <peter@nearprotocol.com>
2020-02-03 15:14:38 -0300

    init

---
+++ fish/config.fish
@@ -0,0 +1,2 @@
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/peterdepaulo/Developer/google-cloud-sdk/path.fish.inc' ]; . '/Users/peterdepaulo/Developer/google-cloud-sdk/path.fish.inc'; end
